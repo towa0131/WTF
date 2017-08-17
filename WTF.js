8b552f0dbb22569949dc0d51ff92ae2366acd1c7
towa0131 <towa.towa0131@icloud.com>
2017-08-17 17:30:56 +0900

    Add Spam MOD

--- WTF.js
+++ WTF.js
@@ -1,4 +1,4 @@
-﻿/* WTF Client by towa0131 */
+/* WTF Client by towa0131 */
 
 /* MainActivity */
 	var ctx = com.mojang.minecraftpe.MainActivity.currentMainActivity.get();
@@ -29,17 +29,17 @@
 
 /* Mods */
 	var spam = false;
-	var spammer = "WTF";
-	var spam_message = "<"+ spammer +"§f> What the Fuck!?";
+	var spam_message = "What the Fuck!?";
 	var tapdestroy = false;
 	var fly = false;
 
 /* Buttons */
 	var ButtonWindow2;
 	var ButtonWindow3;
+	var ButtonWindow4;
 
 /* Button Set */
-		SetButton();
+		SetButton(0);
 
 /* Main Function */
 	function SetButton(option){
@@ -58,10 +58,11 @@
 				DeleteButton();
 				SetButton(1);
 				SetButton(2);
+				SetButton(3);
 				SetButton_Exit();
-				clientMessage("§l§eサーバーの情報");
-				clientMessage("§l§bIP:§a"+ Server.getAddress() + " §bポート:§a"+ Server.getPort());
-				clientMessage("§l§bワールド§a"+ getWorld());
+				var address = Server.getAddress();
+					clientMessage("§l§eサーバーの情報");
+					clientMessage("§l§bIP:§a"+ address + " §bポート:§a"+ Server.getPort());
 			}});
 
 			ButtonLayout.addView(Button);
@@ -139,6 +140,37 @@
 				print("エラーが発生しました" + problem);
 			}}}));
 		break;
+		case 3:
+			ctx.runOnUiThread(new java.lang.Runnable({ run: function() {
+			try{
+			ButtonWindow4 = new android.widget.PopupWindow();
+			ButtonLayout4 = new android.widget.RelativeLayout(ctx);
+			Button = new android.widget.Button(ctx);
+			Button.setText("Spam");
+
+			Button.setOnClickListener(new android.view.View.OnClickListener(){ 
+			onClick: function(view) {
+				if(spam == false){
+					clientMessage("§c[§bWTF§c]§aSpam started!");
+					spam = true;
+					spamtask();
+				}else{
+					clientMessage("§c[§bWTF§c]§bSpam stopped!");
+					spam = false;
+				}
+			}});
+
+			ButtonLayout4.addView(Button);
+			ButtonWindow4.setContentView(ButtonLayout4);
+			ButtonWindow4.setWidth(android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+			ButtonWindow4.setHeight(android.widget.RelativeLayout.LayoutParams.WRAP_CONTENT);
+			ButtonWindow4.setBackgroundDrawable(new android.graphics.drawable.ColorDrawable(android.graphics.Color.TRANSPARENT));
+			ButtonWindow4.showAtLocation(ctx.getWindow().getDecorView(), android.view.Gravity.RIGHT | android.view.Gravity.TOP, 0, 280);
+
+			}catch(problem){
+				print("エラーが発生しました" + problem);
+			}}}));
+		break;
 		}//switch
 	}
 
@@ -154,7 +186,7 @@
 		Button.setOnClickListener(new android.view.View.OnClickListener(){ 
 		onClick: function(view) {
 			DeleteButton();
-			SetButton();
+			SetButton(0);
 		}});
 
 		ButtonLayout.addView(Button);
@@ -182,6 +214,10 @@
 			if(ButtonWindow3 != null) {
 				ButtonWindow3.dismiss();
 				Buttonwindow3 = null;
+			}
+			if(ButtonWindow4 != null) {
+				ButtonWindow4.dismiss();
+				Buttonwindow4 = null;
 			}
 		}}));
 	}
@@ -225,7 +261,7 @@
 			Level.setTile(x,y,z,0,0);
 		}
 	}
-
+	
 	function leaveGame(){
 		loggin = false;
 	}
@@ -242,4 +278,19 @@
 	function getMinecraftVersion(){
 		var mcpeversion = ModPE.getMinecraftVersion();
 		return mcpeversion;
+	}
+	
+	function spamtask() {
+		ctx.runOnUiThread(new java.lang.Runnable({
+  			run: function () {
+  				new android.os.Handler().postDelayed(new java.lang.Runnable({
+  					run: function () {
+  						if (spam){
+  							Server.sendChat(spam_message);
+							eval(spamtask());
+  						}
+  					}
+  				}), 750);
+			}
+		}))
 	}
